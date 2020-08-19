6991a99c66a138e9cd293e8b6cb4c72d445f1ba9
dreamzsameer <sameer20131@gmail.com>
2020-08-19 21:13:10 +0545

    res status code modified

--- Controller/userController.js
+++ Controller/userController.js
@@ -22,12 +22,12 @@ router.post("/register", (req, res) => {
         user
           .save()
           .then((success) => {
-            res.status(201).json({
-              message: "User registered successfully",
+            res.status(200).json({
+              success:true
             });
           })
           .catch((err) => {
-            res.status(500).json({ message: "Error" });
+            res.status(500).json({ success:false });
             console.log(err);
           });
       }
