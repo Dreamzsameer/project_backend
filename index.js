2f484513ac4dae0a389ab2d471ad0be2c7f806c6
Dreamzsameer <sameer20131@gmail.com>
2020-08-21 20:52:16 +0545

    payment

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ app.use(express.static(__dirname + "/assets"));
 app.use(express.static(__dirname + "/assets/images"));
 app.use("/assets/images", express.static("assets/images"));
 app.use(cors());
+const fileUploadRoute = require("./Controller/imageUploadController");
 const userRoute = require("./Controller/userController");
 const productRoute = require("./Controller/productController");
-const fileUploadRoute = require("./Controller/imageUploadController");
+const paymentRoute = require("./Controller/paymentController");
 
+app.use("/upload", fileUploadRoute);
 app.use("/user", userRoute);
 app.use("/product", productRoute);
-app.use("/upload", fileUploadRoute);
+app.use("/payment", paymentRoute);
 
 module.exports = app;
