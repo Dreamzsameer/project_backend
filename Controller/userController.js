32d6d02953ca16c1aed4c7acea668a3a248614fe
Dreamzsameer <sameer20131@gmail.com>
2020-08-21 12:51:51 +0545

    user register/login

--- Controller/userController.js
+++ Controller/userController.js
@@ -4,48 +4,43 @@ const auth = require("../System/authentication");
 const User = require("../Model/userModel");
 
 router.post("/register", (req, res) => {
-  User.find({ email: req.body.email }).exec().then((userData)=>{
-    if (userData.length >= 1) {
-      res.status(201).json({ message: "Email already exists" });
-    } else {
-      const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        contact: req.body.contact,
-        password: req.body.password,
-        address: req.body.address,
-      });
+  User.find({ email: req.body.email })
+    .exec()
+    .then((userData) => {
+      if (userData.length >= 1) {
+        res.status(201).json({ message: "Email already exists" });
+      } else {
+        const user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          contact: req.body.contact,
+          password: req.body.password,
+          address: req.body.address,
+        });
 
-      user.save().then((success)=>{
-        res.json({
-          message:true
-        })
-      }).catch((err)=>console.log(err));
-    } 
-  })
-    
-  
+        user
+          .save()
+          .then((success) => {
+            res.json({
+              message: true,
+            });
+          })
+          .catch((err) => console.log(err));
+      }
+    });
 });
-  
+
 router.post("/login", function (req, res) {
-  User.checkCredentialsDb(
-    req.body.email,
-    req.body.password
+  User.find({ email: req.body.email, password: req.body.password }).then(
+    (result) => {
+      if (result != null) {
+        res.status(201).json({
+          data: result,
+          message: true,
+        });
+      }
+    }
   );
-
-  if (users != null) {
-    const token = await users.generateAuthToken();
-    res.json({
-      token: token,
-      users: users,
-      message: true,
-    });
-  } else {
-    res.json({
-      message: "Invalid Login",
-    });
-  }
 });
-
 
 module.exports = router;
