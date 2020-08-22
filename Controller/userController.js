5a1c55046d0f1fe4d79b19ab35117fe3117fa60c
Dreamzsameer <sameer20131@gmail.com>
2020-08-22 21:46:58 +0545

    login

--- Controller/userController.js
+++ Controller/userController.js
@@ -34,10 +34,10 @@ router.post("/register", (req, res) => {
     });
 });
 
-router.get("/login", function (req, res) {
+router.post("/login", function (req, res) {
   User.find({ email: req.body.email, password: req.body.password })
     .then((result) => {
-      if (result != null) {
+      if (result >= l) {
         res.status(201).json({
           data: result,
           message: "Login Success",
